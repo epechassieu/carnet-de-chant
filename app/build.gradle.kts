0d120fc9e84517175db593aaebe8b99f0185165a
jacqueline R <jacqueline.raynaud@gmail.com>
2026-01-12 17:16:48 +0100

    02 change minsdk to 36 and directories

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "fr.epechassieu.carnetdechant"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "fr.epechassieu.carnetdechant"
